819be7875197aeef7bfe5ade8e6d3b5997166ae2
sophie-wjyang <sophie.wjyang@gmail.com>
2023-03-08 10:58:29 -0500

    Add build folder for Github Pages

---
+++ build/precache-manifest.c3dd0c518fddfbe5e53882b65c975403.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcf4fc8ad500796374b5451a3b4b572e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3f2c0e8ed7168a0672e0",
+    "url": "/static/css/main.7a9724d8.chunk.css"
+  },
+  {
+    "revision": "442586797c9edaaa851b",
+    "url": "/static/js/2.12cc453e.chunk.js"
+  },
+  {
+    "revision": "3f2c0e8ed7168a0672e0",
+    "url": "/static/js/main.7d929c68.chunk.js"
+  },
+  {
+    "revision": "92dcd62a339e58ab19ee",
+    "url": "/static/js/runtime-main.44f53918.js"
+  },
+  {
+    "revision": "ee4bb1423f14f198a08a2bcddab1ea30",
+    "url": "/static/media/waves-background.ee4bb142.svg"
+  }
+]);
